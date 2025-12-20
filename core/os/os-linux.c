38f60c40007b307f92dd48862a8bd2ee9ad6bef9
Cantue35 <71815869+Cantue35@users.noreply.github.com>
2025-12-20 17:27:47 +0300

    Apply stashed local changes

--- core/os/os-linux.c
+++ core/os/os-linux.c
@@ -77,12 +77,6 @@ void *os_alloc_executable(size_t size)
 {
 #if defined(IS_IOS_BUILD)
     void *ptr = mmap((void*)0x0, size, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANON, -1, 0);
-#elif defined(__APPLE__) && defined(__aarch64__) && !defined(IS_IOS_BUILD)
-    /* Apple Silicon macOS requires MAP_JIT + pthread_jit_write_protect_np for JIT code. */
-    #ifndef MAP_JIT
-        #error "MAP_JIT is required for JIT on Apple Silicon macOS"
-    #endif
-    void *ptr = mmap((void*)0x0, size, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_ANON|MAP_JIT, -1, 0);
 #else
     void *ptr = mmap((void*)0x0, size, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_SHARED|MAP_ANON, -1, 0);
 #endif
